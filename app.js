47ff65ce19b7958d7712c9dc472ec161ac030cb6
byambaa <u.bymbaa@gmail.com>
2022-01-01 00:03:45 +0900

    last bullshit hen medhev

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-// toglogchiin eeljiig hadgalah variable, player 1 = 0, player 2 = 1
-
-var activePlayer = 0;
-
-
-// togloogchdiin tsugluulsan onoog hadgalah variable
-
-var scores = [0, 0];
-
-// idevhtei toglochiin eeljiin onoog hadgalah variable
-
-var roundScore = 0;
-
-// shooonii ali talaaraa buusniig hadgalah variable 1-6 gesen utgagiig ene huvisagchid sanamsarguigeer uusgej ugnu
-
-
-//  <div class="player-score" id="score-0">43</div>
-// window.document.querySelector('#score-0').textContent = diceNumber;
-// document.querySelector('#score-1').innerHTML = '<em>yes!</em>';
-
-// programm ehlehed betlgeh
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
-
+var activePlayer, roundScore, scores;
 var diceDom = document.querySelector('.dice');
 diceDom.style.display = 'none';
 
@@ -84,5 +58,25 @@ function switchToNextPlayer (){
 
 //shine togloom ehluuleh tovchnii event listener
 document.querySelector('.btn-new').addEventListener('click', function(){
-    console.log("clicked");
+
 })
+
+function inIt (){
+    // toglogchiin eeljiig hadgalah variable, player 1 = 0, player 2 = 1
+    activePlayer = 0;
+    // togloogchdiin tsugluulsan onoog hadgalah variable
+    scores = [0, 0];
+    // idevhtei toglochiin eeljiin onoog hadgalah variable
+    roundScore = 0;
+    // shooonii ali talaaraa buusniig hadgalah variable 1-6 gesen utgagiig ene huvisagchid sanamsarguigeer uusgej ugnu
+
+    //  <div class="player-score" id="score-0">43</div>
+    // window.document.querySelector('#score-0').textContent = diceNumber;
+    // document.querySelector('#score-1').innerHTML = '<em>yes!</em>';
+
+    // programm ehlehed betlgeh
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+}
